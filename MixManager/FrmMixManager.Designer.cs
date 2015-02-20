65f596deaf10b5e01ab6a48fe841e16e20aaaf71
dubeg <guillaume.dube.girard@gmail.com>
2015-02-20 00:00:02 -0500

    change anchor for btn toggle console

--- MixManager/FrmMixManager.Designer.cs
+++ MixManager/FrmMixManager.Designer.cs
@@ -93,8 +93,7 @@
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(12, 455);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 30);
